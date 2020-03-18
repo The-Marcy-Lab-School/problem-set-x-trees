22872701b5ccc7c91348970ed2f7468ba309c8c9
EC2 Default User <ec2-user@ip-172-31-41-66.ec2.internal>
2020-03-18 04:20:02 +0000

    finished problem-set-11_5

--- exercises.py
+++ exercises.py
@@ -18,31 +18,47 @@ def insert_left(root, val):
     return root
 
 class BinaryTree:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, val):
+        self.val = val
         self.right_child = None
         self.left_child = None
 
-    def insert_right(self, new_node):
+    def insert_right(self, val):
         if self.right_child == None:
-            self.right_child = BinaryTree(new_node)
+            self.right_child = BinaryTree(val)
         else:
-            new_right = BinaryTree(new_node)
-            new_right.right_child = self.right_child
+            old_right = self.right_child
+            new_right = BinaryTree(val)
+            new_right.right_child = old_right
             self.right_child = new_right
         
-    def insert_left(self, new_node):
+    def insert_left(self, val):
         if self.left_child == None:
-            self.left_child = BinaryTree(new_node)
+            self.left_child = BinaryTree(val)
         else:
-            new_left = BinaryTree(new_node)
-            new_left.left_child = self.left_child
+            old_left = self.left_child
+            new_left = BinaryTree(val)
+            new_left.left_child = old_left
             self.left_child = new_left
+            
+    def get_right_child(self):
+        return self.right_child.val
+        
+    def get_left_child(self):
+        return self.left_child.val
+        
+    def set_root_val(self, val):
+        self.val = val
+        return self.val
+        
+    def get_root_val(self):
+        return self.val
+        
         
 def preorder(node):
     if node == None:
         return None
-    print(node.root)
+    print(node.val)
     preorder(node.left_child)
     preorder(node.right_child)
 
@@ -51,17 +67,26 @@ def postorder(node):
         return None
     postorder(node.left_child)
     postorder(node.right_child)
-    print(node.root)
+    print(node.val)
 
 def inorder(node):
     if node == None:
         return None
     inorder(node.left_child)
-    print(node.root)
+    print(node.val)
     inorder(node.right_child)
 
 def is_unival_tree(node):
-    pass
+    values = set()
+	        
+    def traverse(node):
+        if node == None:
+            return None
+        else:
+    	    values.add(node.val)
+    	    traverse(node.left_child)
+    	    traverse(node.right_child)
+    return len(values) == 1
 
 def invert(node):
     if node == None:
@@ -77,5 +102,15 @@ def invert(node):
     
     return node
 
-def second_minimum_node():
-    pass
+def second_minimum_node(node):
+    values = set()
+    
+    def traverse(node):
+        if node == None:
+            return None
+        else:
+            values.add(node.val)
+            traverse(node.left_child)
+            traverse(node.right_child)
+    
+    return sorted(values)[1]
--- test_exercises.py
+++ test_exercises.py
@@ -1,4 +1,4 @@
-from exercises import BinaryTreeList, insert_right, insert_left
+from exercises import BinaryTreeList, insert_right, insert_left, BinaryTree, preorder
 
 def test_BinaryTreeList():
     t = BinaryTreeList('A')
@@ -21,4 +21,34 @@ def test_insert_left():
     insert_left(t, 'Z')
     assert t[1][0] == 'Z'
     assert t[1][1][0] == 'B'
+    
+def test_BinaryTree():
+    t = BinaryTree('A')
+    assert t.right_child == None
+    assert t.left_child == None
+    
+    t.insert_left('B')
+    assert t.left_child.val == 'B'
+    
+    t.insert_right('C')
+    assert t.right_child.val == 'C'
+    
+    t.insert_left('D')
+    assert t.left_child.val == 'D'
+    assert t.left_child.left_child.val == 'B'
+    t.left_child.insert_right('E')
+    assert t.left_child.right_child.val == 'E'
+    
+    t.insert_right('F')
+    assert t.right_child.val == 'F'
+    assert t.right_child.right_child.val == 'C'
+    t.right_child.insert_left('G')
+    assert t.right_child.left_child.val == 'G'
+    
+    assert t.get_left_child() == 'D'
+    assert t.get_right_child() == 'F'
+    
+    assert t.get_root_val() == 'A'
+    t.set_root_val('H')
+    assert t.get_root_val() == 'H'
     
